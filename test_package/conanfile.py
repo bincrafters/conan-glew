cdab9988ab8935d91ace632792aab1248910d738
dimitri <dimi309@users.noreply.github.com>
2018-04-01 08:22:30 +0200

    Testing with older test_package

--- test_package/conanfile.py
+++ test_package/conanfile.py
@@ -1,12 +1,7 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-# vim: tabstop=8 expandtab shiftwidth=4 softtabstop=4
-
-from conans import ConanFile, CMake, tools, RunEnvironment
+from conans import ConanFile, CMake
 import os
 
-
-class TestPackageConan(ConanFile):
+class TestGlew(ConanFile):
     settings = "os", "compiler", "build_type", "arch"
     generators = "cmake"
 
@@ -16,11 +11,11 @@ class TestPackageConan(ConanFile):
         cmake.build()
 
     def test(self):
-        with tools.environment_append(RunEnvironment(self).vars):
-            bin_path = os.path.join("bin", "test_package")
-            if self.settings.os == "Windows":
-                self.run(bin_path)
-            elif self.settings.os == "Macos":
-                self.run("DYLD_LIBRARY_PATH=%s %s" % (os.environ.get('DYLD_LIBRARY_PATH', ''), bin_path))
-            else:
-                self.run("LD_LIBRARY_PATH=%s %s" % (os.environ.get('LD_LIBRARY_PATH', ''), bin_path))
+        self.run(os.sep.join([".","bin", "testGlew"]))
+
+    def imports(self):
+        if self.settings.os == "Windows":
+            self.copy(pattern="*.dll", dst="bin", src="bin")
+            self.copy(pattern="*.pdb", dst="bin", src="bin")
+        if self.settings.os == "Macos":
+            self.copy(pattern="*.dylib", dst="bin", src="lib")
